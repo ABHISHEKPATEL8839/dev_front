e75150d5f242876b5376bfe0546fa99caf72ab2b
ABHISHEKPATEL8839 <abhishekpatel71773@gmail.com>
2026-02-24 16:17:37 +0530

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,44 @@
+// import "./App.css";
+// import Header from "./component/Header";
+// import Box from "@mui/material/Box";
+// import PublicRoutes from "./routes/PublicRoutes";
+// import { BrowserRouter } from "react-router-dom";
+// import Footer from "./component/Footer";
+// import { GlobalCssStyles } from "./style/GlobalCSS";
+// import { LanguageProvider } from "./context/LanguageContext";
+
+// function App() {
+//   const user = JSON?.parse(localStorage.getItem("user"));
+//   return (
+//     <div>
+//       <LanguageProvider>
+//         {/* <ThemeProvider theme={theme}> */}
+//         <GlobalCssStyles />
+//         <Header />
+//         <Box
+//           sx={{
+//             position: "relative",
+//             top: "1%",
+//           }}
+//         >
+//           <PublicRoutes user={user} />
+//         </Box>
+//         {/* <Home /> */}
+//         {/* <Box sx={{minHeight:"190vh", overflowY:'auto'}}>
+
+//       </Box> */}
+//         <Footer />
+//         {/* </ThemeProvider> */}
+//       </LanguageProvider>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
 import "./App.css";
 import Header from "./component/Header";
 import Box from "@mui/material/Box";
@@ -8,31 +49,28 @@ import { GlobalCssStyles } from "./style/GlobalCSS";
 import { LanguageProvider } from "./context/LanguageContext";
 
 function App() {
-  const user = JSON?.parse(localStorage.getItem("user"));
+  const user = JSON.parse(localStorage.getItem("user"));
+
   return (
-    <div>
-      <BrowserRouter>
+    <BrowserRouter>
       <LanguageProvider>
-        {/* <ThemeProvider theme={theme}> */}
         <GlobalCssStyles />
+
         <Header />
+
         <Box
           sx={{
             position: "relative",
             top: "1%",
+            minHeight: "80vh",
           }}
         >
           <PublicRoutes user={user} />
         </Box>
-        {/* <Home /> */}
-        {/* <Box sx={{minHeight:"190vh", overflowY:'auto'}}>
 
-      </Box> */}
         <Footer />
-        {/* </ThemeProvider> */}
       </LanguageProvider>
-      </BrowserRouter>
-    </div>
+    </BrowserRouter>
   );
 }
 
